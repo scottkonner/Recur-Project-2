d3e59aba743c2fd3ca517919d77539fd10e3312e
Scott <scott_koner@yahoo.com>
2022-05-18 20:45:12 -0400

    05 solved

--- problems/05-reverse.js
+++ problems/05-reverse.js
@@ -14,9 +14,39 @@ reverse(""); // ""
 
 
 function reverse(str) {
- ttt
+  debugger;
+//   if( str.length === 0){
+//     return str
+//   }
+//  if (str.length === 1){
+//    return str;
+//  }
+
+// //  let last = str.length-1;
+
+// //  let word = str.slice(0,last);
+// //  let char = str[last]
+// //  const result = char + reverse(word);
+//  return result;
+if ( str.length === 0){
+  return str
+}
+if (str.length === 1){
+  return str
+}
+let first = str[0]
+let letter = str.slice(1, str.length)
+const result =reverse(letter) + first
+return result
 }
 
+
+console.log(reverse("house")); // "esuoh"
+console.log(reverse("dog")); // "god"
+console.log(reverse("atom")); // "mota"
+console.log(reverse("q")); // "q"
+console.log(reverse("id")); // "di"
+console.log(reverse("")); // ""
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
